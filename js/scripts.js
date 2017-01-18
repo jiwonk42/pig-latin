a975ef5eaae4a19874f06bd55e102f90f12c8d01
Jiwon Kang and Nicole Sanders <jiwon@>
2017-01-18 15:42:43 -0800

    keep the letters q and u together and split the word at the next letter after the u

--- js/scripts.js
+++ js/scripts.js
@@ -5,53 +5,24 @@ var pigLatin = function(aWord) {
   aWord = aWord.toLowerCase();
 
   var ending = "ay"
-
   var resultWord = "";
-
-  // vowels.forEach(function(vowel) {
-  //   if(aWord.charAt(0) === vowel) {
-  //     resultWord = aWord + "ay";
-  //   } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) === vowel){
-  //       if (aWord.substring(0, 2) === "qu") {
-  //         resultWord = aWord.slice(2) + aWord.slice(0,2) + "ay";
-  //       } else {
-  //         resultWord = aWord.slice(1) + aWord.slice(0,1) + "ay";
-  //       }
-  //
-  //   } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) !== vowel && aWord.charAt(2) === vowel){
-  //       resultWord = aWord.slice(2) + aWord.slice(0,2) + "ay";
-  //   } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) !== vowel && aWord.charAt(2) !== vowel && aWord.charAt(3) === vowel){
-  //       resultWord = aWord.slice(3) + aWord.slice(0,3) + "ay";
-  //   } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) !== vowel && aWord.charAt(2) !== vowel && aWord.charAt(3) !== vowel && aWord.charAt(4) === vowel){
-  //       resultWord = aWord.slice(4) + aWord.slice(0,4) + "ay";
-  //   }
-  // });
-  // return resultWord;
-
   var wordArray = aWord.split("");
-  var count = 0;
-
   var splitLocation = 0;
-  for (var i = 0; i < wordArray.length; i++){
-    // if(count <= 2) {
-    //   count++;
-      for(var j = 0; j < vowels.length; j++){
 
-        if (wordArray[i] === vowels[j]) {
+  for (var i = 0; i < wordArray.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if (wordArray[i] === vowels[j]) {
+        if(wordArray[i - 1] === "q" && vowels[j] === "u") {
+          splitLocation = wordArray.indexOf(wordArray[i + 1]);
+        } else {
           splitLocation = wordArray.indexOf(vowels[j]);
-          resultWord = aWord.substring(splitLocation) + aWord.substring(0,splitLocation) + ending;
-          return resultWord;
-          break;
         }
-      };
+        resultWord = aWord.substring(splitLocation) + aWord.substring(0,splitLocation) + ending;
+        return resultWord;
+        break;
+      }
+    };
   };
-
-
-
-
-
-
-
 };
 
 $(document).ready(function(){
