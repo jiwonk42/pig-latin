380b9389dd540bcbd52325c8b4ee7a18f568cacc
Jiwon Kang and Nicole Sanders <nicole@>
2017-01-18 11:22:29 -0800

    add if else statements to split the word when the vowel appears as the second through fourth letter

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,22 @@ var pigLatin = function(aWord) {
   var vowels = ['a', 'e', 'i', 'o', 'u'];
   aWord = aWord.toLowerCase();
 
+  var resultWord = "";
+
   vowels.forEach(function(vowel) {
     if(aWord.charAt(0) === vowel) {
-      aWord = aWord + "ay";
+      resultWord = aWord + "ay";
+    } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) === vowel){
+        resultWord = aWord.slice(1) + aWord.slice(0,1) + "ay";
+    } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) !== vowel && aWord.charAt(2) === vowel){
+        resultWord = aWord.slice(2) + aWord.slice(0,2) + "ay";
+    } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) !== vowel && aWord.charAt(2) !== vowel && aWord.charAt(3) === vowel){
+        resultWord = aWord.slice(3) + aWord.slice(0,3) + "ay";
+    } else if (aWord.charAt(0) !== vowel && aWord.charAt(1) !== vowel && aWord.charAt(2) !== vowel && aWord.charAt(3) !== vowel && aWord.charAt(4) === vowel){
+        resultWord = aWord.slice(4) + aWord.slice(0,4) + "ay";
     }
   });
-  return aWord;
+  return resultWord;
 }
 
 $(document).ready(function(){
